cbe5cc5eeeb745dca292bbab3412ec7d051fdd5a
adamregan53 <adamregan53@gmail.com>
2023-05-10 22:14:33 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,154 @@
+##firebase imports
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+from firebase_admin import db
+
+##data processing imports
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+##other imports
+from flask import Flask, request
+import json
+
+
+##init flask app
+app = Flask("PlaceRecommender")
+
+##init firebase
+cred = credentials.Certificate('service-account.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://travelplanner-371416.firebaseio.com'
+})
+db = firestore.client()
+
+
+##place object class
+class Place(object):
+    def __init__(self, docId, placeId, name, types=[]):
+        self.docId = docId
+        self.placeId = placeId
+        self.name = name
+        self.types = types
+
+
+##returns places
+@app.route('/')
+def index():
+    ##init json array which will contain a list of place objects
+    jsonPlacesArray = []
+
+    ##read data from firebase
+    placeDoc = db.collection(u'places').stream()
+
+    ##iterate over firebase data
+    for doc in placeDoc:
+        placeDict = doc.to_dict()
+        place = Place(doc.id, placeDict[u'id'], placeDict[u'name'], placeDict[u'types'])
+        print(f'{place.docId}, {place.placeId}, {place.name}, {place.types}')
+
+        ##convert place into object
+        jsonPlace = {}
+        jsonPlace['docId'] = place.docId
+        jsonPlace['placeId'] = place.placeId
+        jsonPlace['name'] = place.name
+        jsonPlace['types'] = place.types
+        jsonPlacesArray.append(jsonPlace)
+    
+    return {"places": jsonPlacesArray}
+
+
+##returns recommendations
+@app.route('/recommend', methods=['POST'])
+def findRecommendation():
+    places_df = [] ##init places_df dataframe
+    jsonPlacesArray = [] ##init places array from firebase response
+    jsonResponseArray = [] ##init response array return
+
+    placeReceived = Place(docId = request.json[u'docId'], placeId = request.json[u'placeId'], name = request.json[u'name'], types = request.json[u'types'])
+    ##convert place into object
+    jsonReceived = {}
+    jsonReceived['docId'] = placeReceived.docId
+    jsonReceived['placeId'] = placeReceived.placeId
+    jsonReceived['name'] = placeReceived.name
+    jsonReceived['types'] = placeReceived.types
+
+    ##read data from firebase
+    placeDoc = db.collection(u'places').stream()
+
+    ##iterate over firebase data
+    for doc in placeDoc:
+        placeDict = doc.to_dict()
+        place = Place(doc.id, placeDict[u'id'], placeDict[u'name'], placeDict[u'types'])
+
+        ##convert place into object
+        jsonPlace = {}
+        jsonPlace['docId'] = place.docId
+        jsonPlace['placeId'] = place.placeId
+        jsonPlace['name'] = place.name
+        jsonPlace['types'] = place.types
+        jsonPlacesArray.append(jsonPlace)
+    
+    ##append place received from request to places from firebase
+    jsonPlacesArray.append(jsonReceived)
+
+    ##convert to a json array
+    jsonPlacesArrayDump = json.dumps(jsonPlacesArray)
+
+    ##read json into pandas datafram
+    places_df = pd.read_json(jsonPlacesArrayDump)
+    print(places_df)
+
+    ##separate type array values
+    places_df['types'] = places_df['types'].apply(lambda x:' '.join(x))
+    print(places_df)
+
+    ##get place types array and create and array containing each place type
+    cv = CountVectorizer(max_features = 5000, stop_words = 'english')
+    vectors = cv.fit_transform(places_df['types']).toarray()
+    print("Vectors:")
+    print(vectors)
+    print("Shape")
+    print(vectors.shape)
+    print("Features:")
+    print(len(cv.get_feature_names_out()))
+
+    ##stem
+    places_df['types'] = places_df['types'].apply(stem)
+    print("Stem:")
+    print(places_df['types'])
+
+    ##find similarity using cosine_similarity algorithm
+    print("Similarity")
+    similarity = cosine_similarity(vectors)
+    print(similarity[0])
+    print(similarity[0].shape)
+
+    ##find recommendations
+    place_index = places_df[places_df['placeId'] == placeReceived.placeId].index[0]
+    distances = similarity[place_index]
+    places_list = sorted(list(enumerate(distances)), reverse = True, key = lambda x:x[1])[1:4]
+
+    ##convert recommendations to json response
+    for place in places_list:
+        jsonResponse = {}
+        jsonResponse['docId'] = places_df.iloc[place[0]]['docId']
+        jsonResponse['placeId'] = places_df.iloc[place[0]]['placeId']
+        jsonResponse['name'] = places_df.iloc[place[0]]['name']
+        jsonResponseArray.append(jsonResponse)
+    
+    ##return
+    return {"recommendation": jsonResponseArray}
+
+    
+def stem(text):
+    ps = PorterStemmer()
+    y=[]
+    for i in text.split():
+        y.append(ps.stem(i))
+    return " ".join(y)
