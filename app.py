ca67c878d6790b470b1fc3b98f0a6ab8f5219268
adamregan53 <adamregan53@gmail.com>
2023-05-12 20:56:20 +0100

    testinf changes to json response

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ class Place(object):
 @app.route('/')
 def index():
 
-    return {"index"}
+    return "PlaceRecommender"
 
 
 
@@ -56,6 +56,7 @@ def findRecommendation():
 
     placeReceived = Place(docId = request.json[u'docId'], placeId = request.json[u'placeId'], locationRef = request.json[u'locationRef'], name = request.json[u'name'], types = request.json[u'types'])
     ##convert place into object
+
     jsonReceived = {}
     jsonReceived['docId'] = placeReceived.docId
     jsonReceived['placeId'] = placeReceived.placeId
@@ -67,119 +68,79 @@ def findRecommendation():
 
     placeDoc = db.collection(u'locations').document(placeReceived.locationRef).collection(u'places').stream()
 
+    if placeDoc is not None:
 
-    ##iterate over firebase data
-    for doc in placeDoc:
-        placeDict = doc.to_dict()
-        place = Place(doc.id, placeDict[u'id'], placeReceived.locationRef, placeDict[u'name'], placeDict[u'types'])
+        ##iterate over firebase data
+        for doc in placeDoc:
+            placeDict = doc.to_dict()
+            place = Place(doc.id, placeDict[u'id'], placeReceived.locationRef, placeDict[u'name'], placeDict[u'types'])
 
-        ##convert place into object
-        jsonPlace = {}
-        jsonPlace['docId'] = place.docId
-        jsonPlace['placeId'] = place.placeId
-        jsonPlace['locationRef'] = place.locationRef
-        jsonPlace['name'] = place.name
-        jsonPlace['types'] = place.types
-        jsonPlacesArray.append(jsonPlace)
+            ##convert place into object
+            jsonPlace = {}
+            jsonPlace['docId'] = place.docId
+            jsonPlace['placeId'] = place.placeId
+            jsonPlace['locationRef'] = place.locationRef
+            jsonPlace['name'] = place.name
+            jsonPlace['types'] = place.types
+            jsonPlacesArray.append(jsonPlace)
+        
+        ##append place received from request to places from firebase
+        jsonPlacesArray.append(jsonReceived)
+
+        ##convert to a json array
+        jsonPlacesArrayDump = json.dumps(jsonPlacesArray)
+
+        ##read json into pandas datafram
+        places_df = pd.read_json(jsonPlacesArrayDump)
+        print(places_df)
+
+        ##remove duplicated on id column
+        places_df.drop_duplicates(subset = ["placeId"])
+        print(places_df)
+
+        ##separate type array values
+        places_df['types'] = places_df['types'].apply(lambda x:' '.join(x))
+        print(places_df)
+
+        ##get place types array and create and array containing each place type
+        cv = CountVectorizer(max_features = 5000, stop_words = 'english')
+        vectors = cv.fit_transform(places_df['types']).toarray()
+        print("Vectors:")
+        print(vectors)
+        print("Shape")
+        print(vectors.shape)
+        print("Features:")
+        print(len(cv.get_feature_names_out()))
+
+        ##stem
+        places_df['types'] = places_df['types'].apply(stem)
+        print("Stem:")
+        print(places_df['types'])
+
+        ##find similarity using cosine_similarity algorithm
+        print("Similarity")
+        similarity = cosine_similarity(vectors)
+        print(similarity[0])
+        print(similarity[0].shape)
+
+        ##find recommendations
+        place_index = places_df[places_df['placeId'] == placeReceived.placeId].index[0]
+        distances = similarity[place_index]
+        places_list = sorted(list(enumerate(distances)), reverse = True, key = lambda x:x[1])[1:4]
+
+        ##convert recommendations to json response
+        for place in places_list:
+            jsonResponse = {}
+            jsonResponse['docId'] = places_df.iloc[place[0]]['docId']
+            jsonResponse['placeId'] = places_df.iloc[place[0]]['placeId']
+            jsonResponse['name'] = places_df.iloc[place[0]]['name']
+            jsonResponseArray.append(jsonResponse)
+        
+        ##return
+        return jsonResponseArray
     
-    ##append place received from request to places from firebase
-    jsonPlacesArray.append(jsonReceived)
-
-    ##convert to a json array
-    jsonPlacesArrayDump = json.dumps(jsonPlacesArray)
-
-    ##read json into pandas datafram
-    places_df = pd.read_json(jsonPlacesArrayDump)
-    print(places_df)
-
-    ##remove duplicated on id column
-    places_df.drop_duplicates(subset = ["placeId"])
-    print(places_df)
-
-    ##separate type array values
-    places_df['types'] = places_df['types'].apply(lambda x:' '.join(x))
-    print(places_df)
-
-    ##get place types array and create and array containing each place type
-    cv = CountVectorizer(max_features = 5000, stop_words = 'english')
-    vectors = cv.fit_transform(places_df['types']).toarray()
-    print("Vectors:")
-    print(vectors)
-    print("Shape")
-    print(vectors.shape)
-    print("Features:")
-    print(len(cv.get_feature_names_out()))
-
-    ##stem
-    places_df['types'] = places_df['types'].apply(stem)
-    print("Stem:")
-    print(places_df['types'])
-
-    ##find similarity using cosine_similarity algorithm
-    print("Similarity")
-    similarity = cosine_similarity(vectors)
-    print(similarity[0])
-    print(similarity[0].shape)
-
-    ##find recommendations
-    place_index = places_df[places_df['placeId'] == placeReceived.placeId].index[0]
-    distances = similarity[place_index]
-    places_list = sorted(list(enumerate(distances)), reverse = True, key = lambda x:x[1])[1:4]
-
-    ##convert recommendations to json response
-    for place in places_list:
-        jsonResponse = {}
-        jsonResponse['docId'] = places_df.iloc[place[0]]['docId']
-        jsonResponse['placeId'] = places_df.iloc[place[0]]['placeId']
-        jsonResponse['name'] = places_df.iloc[place[0]]['name']
-        jsonResponseArray.append(jsonResponse)
-    
-    ##return
-    return {"recommendation": jsonResponseArray}
-
-
-
-
-@app.route('/test', methods=['POST'])
-def testRecommend():
-    jsonPlacesArray = [] ##init places array from firebase response
-    jsonResponseArray = [] ##init response array return
-
-    placeReceived = Place(docId = request.json[u'docId'], placeId = request.json[u'placeId'], locationRef = request.json[u'locationRef'], name = request.json[u'name'], types = request.json[u'types'])
-    ##convert place into object
-    print("Recieved from Post Request")
-    jsonReceived = {}
-    jsonReceived['docId'] = placeReceived.docId
-    jsonReceived['placeId'] = placeReceived.placeId
-    jsonReceived['locationRef'] = placeReceived.locationRef
-    jsonReceived['name'] = placeReceived.name
-    jsonReceived['types'] = placeReceived.types
-    print(jsonReceived)
-
-    ##read data from firebase
-    placeDoc = db.collection(u'locations').document(placeReceived.locationRef).collection(u'places').stream()
-
-    ##iterate over firebase data
-    print("Received from firebase")
-    for doc in placeDoc:
-        placeDict = doc.to_dict()
-        print(placeDict)
-        place = Place(doc.id, placeDict[u'id'], placeReceived.locationRef,placeDict[u'name'], placeDict[u'types'])
-        print(place)
-
-        ##convert place into object
-        jsonPlace = {}
-        jsonPlace['docId'] = place.docId
-        jsonPlace['placeId'] = place.placeId
-        jsonPlace['locationRef'] = place.locationRef
-        jsonPlace['name'] = place.name
-        jsonPlace['types'] = place.types
-        jsonPlacesArray.append(jsonPlace)
-        print(jsonPlacesArray)
-
-    return {"test": jsonPlacesArray}
-
-
+    else:
+        return {}
 
     
 def stem(text):
